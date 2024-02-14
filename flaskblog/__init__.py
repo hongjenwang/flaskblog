6c63c65e9760201e02629a4d44c365bbd8a692c2
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-02-13 23:33:00 -0800

    add forgot password functionality

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,12 @@
+from dotenv import load_dotenv
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
-
+load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '854ab6876cb124cdd0df2e1ce75edbd1'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
@@ -12,5 +15,11 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
 from flaskblog import routes
